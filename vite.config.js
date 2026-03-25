63dcefa80de451cce692c849417d43d00b1ec1cb
Your Name <you@example.com>
2026-03-25 17:25:42 +0800

    add element-plus

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,14 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  },
+  build: {
+    rollupOptions: {
+      input: 'index.html'
+    }
+  }
 })
